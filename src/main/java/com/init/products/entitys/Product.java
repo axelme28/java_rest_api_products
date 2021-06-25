a8887dbfbd2bc4eb1f196d447f2ab8a74519be8f
axelme28 <a.mendez.m@edu.utc.mx>
2021-06-24 20:41:31 -0500

    api terminada

--- src/main/java/com/init/products/entitys/Product.java
+++ src/main/java/com/init/products/entitys/Product.java
@@ -1,11 +1,15 @@
 package com.init.products.entitys;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+//jdbc:mysql://localhost:3306/productos?zeroDateTimeBehavior=convertToNull&serverTimezone=UTC
 @Entity
 @Table(name = "producto")
 public class Product { 
@@ -13,17 +17,57 @@ public class Product {
 	@Id
 	@Column(name = "id_producto")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private int id;
 	
-	@Column(name = " nom_producto", nullable = false, length = 30)
-	private String name;
+	@Column(name = "nom_producto", nullable = false, length = 30)
+	private String nombre;
 	
-	@Column(name = "id_provedor")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id_provedor;
+	@Column(name = "stock")
+	private int stock;
 	
-	@Column(name = "id_modelo")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id_modelo;
+	@Column(name = "nom_provedor", length = 30)
+	private String  nom_provedor;
+	
+	@Column(name = "no_modelo", length = 30)
+	private String  no_modelo;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getNom_provedor() {
+		return nom_provedor;
+	}
+
+	public void setNom_provedor(String nom_provedor) {
+		this.nom_provedor = nom_provedor;
+	}
+
+	public String getNo_modelo() {
+		return no_modelo;
+	}
+
+	public void setNo_modelo(String no_modelo) {
+		this.no_modelo = no_modelo;
+	}
 }
--- src/main/java/com/init/products/rest/ProductsREST.java
+++ src/main/java/com/init/products/rest/ProductsREST.java
@@ -1,22 +1,78 @@
 package com.init.products.rest;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.init.products.dao.ProductsDAO;
 import com.init.products.entitys.Product;
 
 
 @RestController
 @RequestMapping("products")
 public class ProductsREST {
+	@Autowired 
+	private ProductsDAO productDAO;
 	
 	@GetMapping
 	public ResponseEntity<List<Product>> getProduct(){
-		return null;
+		List<Product> products = productDAO.findAll();
+		return ResponseEntity.ok(products);
 	}
+	
+	//servicio para buscar un articulo por el id
+	@RequestMapping(value = "{id_producto}")
+	public ResponseEntity<Product> getProduct(@PathVariable("id_producto") int productid){
+		Optional<Product> opcionalProduct = productDAO.findById(productid);
+		if(opcionalProduct.isPresent()) {
+			return ResponseEntity.ok(opcionalProduct.get());
+		}else {
+			return ResponseEntity.noContent().build();
+		}
+	}
+	
+	
+	//Servicio para agregar un nuevo producto
+	@PostMapping // /products (POST)
+	public ResponseEntity<Product> createProduct (@RequestBody Product product){
+		Product newproduct = productDAO.save(product);
+		return ResponseEntity.ok(newproduct);
+	}
+	
+	//Servicio para borrar un producto
+	@DeleteMapping(value = "{id_producto}") // /products (DELETE)
+	public ResponseEntity<Void> deleteProduct (@PathVariable("id_producto") int productid){
+		productDAO.deleteById(productid);
+		return ResponseEntity.ok(null);
+		
+	}
+	
+	//Servicio para actualizar un producto
+	@PutMapping // /products (UPDATE)
+	public ResponseEntity<Product> updateProduct(@RequestBody Product product){
+		Optional<Product> optionalProduct = productDAO.findById(product.getId());
+		if(optionalProduct.isPresent()) {
+			Product updateProduct = optionalProduct.get();
+			updateProduct.setNo_modelo(product.getNo_modelo());
+			updateProduct.setNom_provedor(product.getNom_provedor());
+			updateProduct.setNombre(product.getNombre());
+			updateProduct.setStock(product.getStock());
+			return ResponseEntity.ok(updateProduct);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
 		 
 	
 }
